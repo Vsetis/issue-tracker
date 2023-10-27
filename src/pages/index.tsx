b01ccd61daa7fb0ca783cbdc4d6b4f3940d8c70e
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-10-27 19:47:05 +0200

    remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@radix-ui/themes";
-import Head from "next/head";
-import Link from "next/link";
-
-import { api } from "~/utils/api";
-
 export default function Home() {
   return (
     <>
