22f5483c10ad887ed7143d7904f65b5ede5c7eeb
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-10-27 19:53:50 +0200

    remove unused post

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { issueRouter } from "./routers/issue";
 
@@ -8,7 +7,6 @@ import { issueRouter } from "./routers/issue";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   issue: issueRouter,
 });
 
