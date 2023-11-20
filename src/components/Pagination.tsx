0d8d0a6fbd84c753add60c85fbbd371da23110d1
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-11-20 10:24:00 +0100

    update pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import { IconChevronsRight, IconChevronsLeft } from "@tabler/icons-react";
+
 export default function Pagination({
   lastPage,
   currentPage,
@@ -11,18 +13,22 @@ export default function Pagination({
 }) {
   const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
 
+  if (totalPages === 1) return null;
+
   return (
     <nav>
-      <ul className="m-0 mt-4 flex list-none gap-4 p-0 text-lg font-semibold">
+      <ul className="m-0 mt-4 flex list-none flex-wrap gap-4 p-0 text-lg font-semibold">
         <li
-          className="cursor-pointer rounded bg-black/10 px-2"
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded border"
           onClick={() => onPageChange(1)}
-        >{`<<`}</li>
+        >
+          <IconChevronsLeft className="w-4" />
+        </li>
         {pages.map((page) => (
           <li
             className={`${
-              page === currentPage ? "bg-black text-white" : ""
-            } cursor-pointer rounded bg-black/10 px-2 transition-all`}
+              page === currentPage ? "bg-[#5B5BD6] text-white" : ""
+            } flex h-8 w-8 cursor-pointer items-center justify-center rounded border text-sm transition-all`}
             key={page}
             onClick={() => onPageChange(page)}
           >
@@ -30,9 +36,11 @@ export default function Pagination({
           </li>
         ))}
         <li
-          className="cursor-pointer rounded bg-black/10 px-2"
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded border"
           onClick={() => onPageChange(lastPage)}
-        >{`>>`}</li>
+        >
+          <IconChevronsRight className="w-4" />
+        </li>
       </ul>
     </nav>
   );
